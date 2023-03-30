0574b37417a3b44d339e8d27dba3e45d359286a7
ruby0701 <ruby0701@gmail.com>
2023-03-30 20:37:24 +0800

    implements the app.js file

--- app.js
+++ app.js
@@ -10,48 +10,61 @@ const player2score = document.querySelector("#player2score");
 let score1 = parseInt(player1score.innerText);
 let score2 = parseInt(player2score.innerText);
 
-let winnignScore = 5;
+const scoreTop = document.querySelector("#scoreTop");
+
+let winningScore = parseInt(scoreTop.value);
 let isGameOver = false;
 
 player1.addEventListener("click", () => {
 
-
     //making sure the game dosen't continue once a player hits the winning score
     if (!isGameOver) {
         //setting like this can prevent the score go over winning score
-        if (score1 !== winnignScore) {
+        if (score1 !== winningScore) {
             score1 += 1;
             player1score.innerText = score1;
 
         }
     }
-    if (score1 === winnignScore) {
+    if (score1 === winningScore) {
         isGameOver = true;
     }
-
-
-    //player1score.innerHTML = `<h1 color="green">${score}</h1>`;
 });
 
 player2.addEventListener("click", () => {
 
-
     if (!isGameOver) {
         //setting like this can prevent the score go over winning score
-        if (score2 !== winnignScore) {
+        if (score2 !== winningScore) {
             score2 += 1;
             player2score.innerText = score2;
 
         }
     }
-    if (score2 === winnignScore) {
+    if (score2 === winningScore) {
         isGameOver = true;
     }
-
-    //player1score.innerHTML = `<h1 color="green">${score}</h1>`;
 });
 
 reset.addEventListener("click", () => {
     player1score.innerText = "0";
     player2score.innerText = "0";
+    score1 = 0;
+    score2 = 0;
+    isGameOver = false;
+});
+
+scoreTop.addEventListener("change", () => {
+    //get top score and store in winnigScore
+    winningScore = parseInt(scoreTop.value);
+
+    //needs to reset the scores so the game restart
+    //you don't change the top score mid-game
+    player1score.innerText = "0";
+    player2score.innerText = "0";
+    score1 = 0;
+    score2 = 0;
+    isGameOver = false;
+
+
 });
